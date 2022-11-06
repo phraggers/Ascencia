9bc3f801c57e0b8db62670f649e4eacd8e168ce8
phraggers <phil@prbagshaw.com>
2022-11-06 05:56:31 +0000

    moved git dir up 1 level

---
+++ src/Audio.c
@@ -0,0 +1,29 @@
+
+#include "Ascencia.h"
+
+r32 Audio_GetMusicVolume(void)
+{
+	if (!State) return 0;
+
+	int Volume = Mix_VolumeMusic(-1);
+	State->Audio.MusicVolume = (r32)((r32)Volume / 128.0f);
+	return State->Audio.MusicVolume;
+}
+
+void Audio_SetMusicVolume(r32 _Volume)
+{
+	if (!State) return;
+
+	int Volume;
+
+	if (_Volume <= 0.0f) Volume = 0;
+	else if (_Volume >= 1.0f) Volume = 128;
+	else Volume = (int)((r32)128.0f * (r32)_Volume);
+
+	State->Audio.MusicVolume = (r32)Volume / 128.0f;
+	Mix_VolumeMusic(Volume);
+}
+
+// chunk settings
+//Mix_Volume(_Channel, _Volume); //channel -1 for all (int, int)
+//Mix_SetPanning(_Channel, _Left, _Right); //(int, Uint8, Uint8) _Channel = MIX_CHANNEL_POST means pan happens last
---
+++ src/Window.h
@@ -0,0 +1,28 @@
+
+#ifndef _ASC_WINDOW_H_
+#define _ASC_WINDOW_H_
+
+typedef struct
+{
+	char Title[64];
+	SDL_Window* Handle;
+	SDL_GLContext GLContext;
+	SDL_Rect Dimensions;
+	SDL_Cursor* Cursor;
+	bool Exposed;
+	bool Minimized;
+	bool Maximized;
+	bool MouseFocus;
+	bool KeyboardFocus;
+	bool Fullscreen;
+	bool MouseShown;
+} ASC_Window;
+
+bool Window_Init(void);
+void Window_Resize(void);
+bool Window_ToggleFullscreen(void);
+bool Window_Screenshot(char* _Path); // can pass 0 if no path needed
+bool Window_ToggleMouse(void);
+SDL_Cursor* Window_SDLCursor(void);
+
+#endif //_ASC_WINDOW_H_
